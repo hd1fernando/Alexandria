174a57b59b5889f98e3767bdfa1ea3aa08cb001e
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-08-02 19:57:20 -0300

    :bug: remoção de \r em scripts para inicializar bd

--- data/db-init.sql
+++ data/db-init.sql
@@ -2,7 +2,7 @@ USE [master]
 GO
 
 IF DB_ID('Alexandria') IS NOT NULL
-	  set noexec on               -- prevent creation when already exists
+	set noexec on               -- prevent creation when already exists
 
 CREATE DATABASE [MercadoLivreClone];
 GO
