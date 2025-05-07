1842245a521e5f920eac1d15106aae5e49b2de90
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2025-05-07 18:47:40 -0300

    feat: add validation to dto input

--- Alexandria.Api/Controllers/Dtos/CreateAuthorRequest.cs
+++ Alexandria.Api/Controllers/Dtos/CreateAuthorRequest.cs
@@ -1,8 +1,14 @@
-﻿namespace Alexandria.Api.Controllers.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Alexandria.Api.Controllers.Dtos;
 
 public class CreateAuthorRequest
 {
+    [Required(ErrorMessage = "{0} is required")]
     public string? Name { get; set; }
+    [Required(ErrorMessage = "{0} is required")]
     public string? Email { get; set; }
+    [Required(ErrorMessage = "{0} is required")]
+    [MaxLength(400, ErrorMessage = "{0} must be less than {1} characters")]
     public string? Description { get; set; }
 }
